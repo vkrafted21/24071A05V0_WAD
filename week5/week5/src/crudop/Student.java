59713677610ad995a5cd83b963926c03d95876cd
vkrafted21 <vaishnavikoppakula@gmail.com>
2026-02-03 11:45:29 +0530

    crudop-5

---
+++ week5/week5/src/crudop/Student.java
@@ -0,0 +1,14 @@
+package crudop;
+
+public class Student {
+    int id;
+    String name;
+    String email;
+    int age;
+
+    public Student(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+}
